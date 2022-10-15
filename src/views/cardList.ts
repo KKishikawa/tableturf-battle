85f5631af9eabe9a2ed907b4baa20afd8fdb540e
Kenta Kishikawa <k1saku.philosophyz@gmail.com>
2022-10-15 23:51:58 +0900

    タブ切り替え実装 カードプレビューの位置調整

--- src/views/cardList.ts
+++ src/views/cardList.ts
@@ -3,8 +3,28 @@ import { getCardList } from "@/models/card";
 
 const allCardManager = new CardList(true);
 allCardManager.addRow(...getCardList().c);
+allCardManager.wrapper.classList.add("deck-tab-item--active");
 const deckManager = new CardList(false);
 
-document
-  .querySelector(".deck-tab-groups")!
-  .append(allCardManager.wrapper, deckManager.wrapper);
+const tabGroup = document.querySelector(".deck-tab-items") as HTMLElement;
+tabGroup.append(allCardManager.wrapper, deckManager.wrapper);
+const tabItems = Array.from(tabGroup.querySelectorAll(".deck-tab-item"));
+const tabBtns = Array.from(document.querySelectorAll(".tab-group .tab"));
+tabBtns.forEach((tabBtn, idx) =>
+  tabBtn.addEventListener("click", () => {
+    tabBtns.forEach((b, bIdx) => {
+      if (bIdx == idx) {
+        b.classList.add("active");
+      } else {
+        b.classList.remove("active");
+      }
+    });
+    tabItems.forEach((tabItem, itemIdx) => {
+      if (idx == itemIdx) {
+        tabItem.classList.add("deck-tab-item--active");
+      } else {
+        tabItem.classList.remove("deck-tab-item--active");
+      }
+    });
+  })
+);
