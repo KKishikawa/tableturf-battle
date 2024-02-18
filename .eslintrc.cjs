9b83cf4c44fd9a71236319a02a49a453b937c6f5
Kenta Kishikawa <k1saku.philosophyz@gmail.com>
2024-02-18 10:47:50 +0900

    chore: add eslint ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended' // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true // Allows for the parsing of JSX
