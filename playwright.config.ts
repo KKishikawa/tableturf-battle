989f93bac72e5d09fb2cad839f3179bd68451127
Kenta Kishikawa <k1saku.philosophyz@gmail.com>
2024-02-19 02:47:31 +0900

    fix: format

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.GITHUB_ACTIONS ? 'github' :'list',
+  reporter: process.env.GITHUB_ACTIONS ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
