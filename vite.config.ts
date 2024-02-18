db6fe00b51012dfaa915bd879f8b7fe4d9febf32
Kenta Kishikawa <k1saku.philosophyz@gmail.com>
2024-02-18 09:12:19 +0900

    chore: migrate pwa conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 import { join } from 'node:path';
+import PWA from './plugins/pwa';
 
 /**@see {@link https://vitejs.dev/config} */
-export default defineConfig(({ mode }) => ({
-  define: {
-    'import.meta.vitest': 'undefined',
-    NODE_ENV: `"${mode}"`,
-  },
-  publicDir: 'public',
-  resolve: {
-    alias: {
-      '@/': join(__dirname, 'src/')
-    }
-  },
-}));
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    define: {
+      'import.meta.vitest': 'undefined',
+      NODE_ENV: `"${mode}"`,
+    },
+    publicDir: 'public',
+    base: process.env.VITE_APP_PATH,
+    resolve: {
+      alias: {
+        '@/': join(__dirname, 'src/')
+      }
+    },
+    plugins: [
+      PWA(),
+    ]
+  }
+});
