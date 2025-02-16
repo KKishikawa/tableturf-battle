36f080fe56088f1151b64d216fbb2a5d2d6c2e6a
Kenta Kishikawa <k1saku.philosophyz@gmail.com>
2025-02-16 21:24:50 +0900

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 import istanbul from 'vite-plugin-istanbul';
 
 import PWA from './plugins/pwa';
